c4ed5232325370beee359b210a53149ab59dfa17
Gonzales Walisson <walisson.gonzales@gmail.com>
2021-08-18 17:42:41 -0300

    Iniciando Projeto

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="toolbar">
+                <div className="">
+                    <span className="">Gonz Music</span>
+                </div>
+
+                <div className="">
+                    <button>Novo Post</button>
+                    <span>Img1</span>
+                    <span>img2</span>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
